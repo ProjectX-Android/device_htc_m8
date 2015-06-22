9a4f2fbd840bc8e2f811b87b62bb3b07f145d0db
Paul Beeler <pbeeler80@gmail.com>
2015-06-21 22:27:14 -0600

    post-init.sh: Google play services wakelock fix
    Credit to XileForce and LSpeed Mod
    
    Signed-off-by: Paul Beeler <pbeeler80@gmail.com>
    
    Conflicts:
    	init.hammerhead.rc
    
    Change-Id: Id8e03829f883d7e848e4d36284e8f8509cc1bae8

---
+++ post-init.sh
@@ -0,0 +1,17 @@
+#Fix GPS Wake Issues. From LSpeed Mod 
+busybox mount -o remount,rw rootfs
+busybox mount -o remount,rw /system
+
+su -c "pm enable com.google.android.gms/.update.SystemUpdateActivity"
+su -c "pm enable com.google.android.gms/.update.SystemUpdateService"
+su -c "pm enable com.google.android.gms/.update.SystemUpdateService$ActiveReceiver"
+su -c "pm enable com.google.android.gms/.update.SystemUpdateService$Receiver"
+su -c "pm enable com.google.android.gms/.update.SystemUpdateService$SecretCodeReceiver"
+su -c "pm enable com.google.android.gsf/.update.SystemUpdateActivity"
+su -c "pm enable com.google.android.gsf/.update.SystemUpdatePanoActivity"
+su -c "pm enable com.google.android.gsf/.update.SystemUpdateService"
+su -c "pm enable com.google.android.gsf/.update.SystemUpdateService$Receiver"
+su -c "pm enable com.google.android.gsf/.update.SystemUpdateService$SecretCodeReceiver"
+
+busybox mount -o remount,ro rootfs
+busybox mount -o remount,ro /system
